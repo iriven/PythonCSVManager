c4db78cc4580b11534a1d7f67cdd5db0c3e1b7d0
Alfred Tchondjo <iriven@yahoo.fr>
2019-01-12 14:09:45 +0100

    Update Examples.py

--- Examples/Csv/Examples.py
+++ Examples/Csv/Examples.py
@@ -1,1 +1,25 @@
+import sys 
+import os
+PackageDir = "./IrivenPythonTools"
+sys.path.append(os.path.abspath(PackageDir))
+from IrivenBootstrap import *
+if __name__ == "__main__":
+    fileloader = Iriven_CSVParser("./testfile.csv")
+    fileloader.setDelimiter(":")
+    fileloader.parse()
+    print(fileloader.getNumRows())
+    print(fileloader.fetchRows())
+    print(fileloader.getRows())
+    CSVParserProp = Iriven_ClassInfos(fileloader)
+    print(CSVParserProp.GetMethods())
 
+    filecreator = Iriven_CSVDumper()
+    filecreator.setDestination('./fileCreated1.csv')
+    filecreator.save(fileloader.fetchRows(("osadmin" , "root")))
+    s = "----------------------------------------------------------" + eol
+    for row in fileloader.fetchRows(("osadmin" , "admplat")):
+        s += "Server : " + row[0] + eol
+        s += "User : " + row[1] + eol
+        s += "Password : " + row[2] + eol
+        s += "----------------------------------------------------------" + eol
+        print(s)
