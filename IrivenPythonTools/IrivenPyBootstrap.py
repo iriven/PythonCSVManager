117c83cbba61d5ba89ec45a1f629fd8122be0a17
Alfred Tchondjo <iriven@yahoo.fr>
2019-01-12 13:43:09 +0100

    Update IrivenPyBootstrap.py

--- IrivenPythonTools/IrivenPyBootstrap.py
+++ IrivenPythonTools/IrivenPyBootstrap.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.getcwd()))
 
 from Libraries.ClsProperties import *
 from Libraries.CsvParserFile import *
-from Libraries.CsvDumperFile import *
+from Libraries.CSVDumperClass import *
 eol = os.linesep
 
 if __name__ == "__main__":
