862f5e717f4c6a320b383b2f44ff087497abee98
Alfred Tchondjo <iriven@yahoo.fr>
2019-01-12 13:45:39 +0100

    Update IrivenPyBootstrap.py

--- IrivenPythonTools/IrivenPyBootstrap.py
+++ IrivenPythonTools/IrivenPyBootstrap.py
@@ -3,7 +3,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.getcwd()))
 
-from Libraries.ClsProperties import *
+from Libraries.ReflectionClass import *
 from Libraries.CsvParserFile import *
 from Libraries.CSVDumperClass import *
 eol = os.linesep
