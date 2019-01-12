c57a3479a8327700f9eba7e985e8c8b3ae12c83e
Alfred Tchondjo <iriven@yahoo.fr>
2019-01-12 13:31:07 +0100

    Update IrivenPyBootstrap.py

--- IrivenPyBootstrap.py
+++ IrivenPyBootstrap.py
@@ -1,1 +1,12 @@
+#coding: utf-8
+import sys 
+import os
+sys.path.append(os.path.dirname(os.getcwd()))
 
+from Libraries.ClsProperties import *
+from Libraries.CsvParserFile import *
+from Libraries.CsvDumperFile import *
+eol = os.linesep
+
+if __name__ == "__main__":
+    sys.exit(main())
