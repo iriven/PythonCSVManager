197a04cf593ed3d846c0d82a01b2059a7b5baead
Alfred Tchondjo <iriven@yahoo.fr>
2019-01-12 13:36:19 +0100

    Create ClsProperties.py

---
+++ IrivenPythonTools/Libraries/ClsProperties.py
@@ -0,0 +1,21 @@
+#coding: utf-8
+import inspect
+class Iriven_ClassInfos(object):
+    def __init__(self, clsobj):
+        self.clsobj = clsobj
+
+    def GetProperties(self):
+        prop = {}
+        for name in dir(self.clsobj):
+            value = getattr(self.clsobj, name)
+            if not name.startswith('__') and not inspect.ismethod(value):
+                prop[name] = value
+        return prop
+    
+    def GetMethods(self):
+        method = {}
+        for name in dir(self.clsobj):
+            attr = getattr(self.clsobj, name)
+            if not name.startswith('__') and inspect.ismethod(attr):
+                method[name] = attr
+        return method
