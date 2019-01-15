4537c1a3f1cd57c4d04c86082b8ead6d7739b127
Alfred Tchondjo <iriven@yahoo.fr>
2019-01-15 21:15:15 +0100

    Update IrivenPyBootstrap.py

--- IrivenPythonTools/IrivenPyBootstrap.py
+++ IrivenPythonTools/IrivenPyBootstrap.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 import sys 
 import os
-sys.path.append(os.path.dirname(os.getcwd()))
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 from Libraries.ReflectionClass import *
 from Libraries.CSVParserClass import *
