d91333c591f0b1d7b45099e8622d26d7c6fb4a98
Alfred Tchondjo <iriven@yahoo.fr>
2019-01-15 21:13:50 +0100

    Update ReflectionClass.py

--- IrivenPythonTools/Libraries/ReflectionClass.py
+++ IrivenPythonTools/Libraries/ReflectionClass.py
@@ -19,7 +19,25 @@ class Iriven_ReflectionClass(object):
             if not name.startswith('__') and inspect.ismethod(attr):
                 method[name] = attr
         return method
-    
+    '''
+    def all_members(self, aClass):
+        try:
+            # Try getting all relevant classes in method-resolution order
+            mro = list(aClass.__mro__)
+        except AttributeError:
+            # If a class has no _ _mro_ _, then it's a classic class
+            def getmro(aClass, recurse):
+                mro = [aClass]
+                for base in aClass.__base__: 
+                    mro.extend(recurse(base, recurse))
+                return mro
+            mro = getmro(aClass, getmro)
+        mro.reverse()
+        members = {}
+        for someClass in mro: 
+            members.update(vars(someClass))
+        return members
+    '''
     
 if __name__ == "__main__":
     sys.exit(main())
