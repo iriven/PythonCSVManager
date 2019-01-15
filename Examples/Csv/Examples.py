e6788165dc0531a3b95820f6d54a6dd5aa440818
Alfred Tchondjo <iriven@yahoo.fr>
2019-01-15 20:58:13 +0100

    Update Examples.py

--- Examples/Csv/Examples.py
+++ Examples/Csv/Examples.py
@@ -1,37 +1,48 @@
 #coding: utf-8
 import sys 
 import os
-IrivenPackageLocation = "../../IrivenPythonTools"
+currentdir = os.path.dirname(os.path.realpath(__file__))
+RootDirectory = os.path.dirname(os.path.dirname(currentdir))
+IrivenPackageLocation = os.path.join(RootDirectory , "IrivenPythonTools")
 sys.path.append(os.path.abspath(IrivenPackageLocation))
+
 from IrivenPyBootstrap import *
+
 if __name__ == "__main__":
-    fileloader = Iriven_CSVParser("./testFile.csv")
+    ''' 
+    get the csv file full path 
+    '''
+    csvfilePath = os.path.join(currentdir , "Inputs",  "testFile.csv") 
+    ParserObject = Iriven_CSVParser(csvfilePath)
     '''
     to test with a non standard delimiter, just call the setDelimiter() method:
-    fileloader = Iriven_CSVParser("./nonStd_testFile.csv")
-    fileloader.setDelimiter(":")
+    ParserObject = Iriven_CSVParser(os.path.join(currentdir , "nonStd_testFile.csv"))
+    ParserObject.setDelimiter(":")
     '''
-    fileloader.parse()
-    print(fileloader.getNumRows())
-    print(fileloader.fetchRows(("user1" , "admin","staff")))
-    print(fileloader.getRows())
+    ParserObject.prepare()
+    ParserObject.load(os.path.join(currentdir ,'Inputs', 'nonStd_testFile.csv'),':')
+    print(ParserObject.getNumRows())
+    print(ParserObject.searchRows(("user1" , "admin")))
+    print(ParserObject.traverse())
     
     '''
     the Reflection Class
     '''
-    CSVParserinfos = Iriven_ReflectionClass(fileloader)
+    CSVParserinfos = Iriven_ReflectionClass(ParserObject)
     print(CSVParserinfos.GetMethods())
     
     '''
     the Dumper Class
     '''
-    filecreator = Iriven_CSVDumper()
-    filecreator.setDestination('./CreatedFile.csv')
-    filecreator.save(fileloader.fetchRows(("admin" , "root")))
-    s = "----------------------------------------------------------" + eol
-    for row in fileloader.fetchRows(("admin","staff")):
-        s += "Server : " + row[0] + eol
-        s += "User : " + row[1] + eol
-        s += "Password : " + row[2] + eol
-        s += "----------------------------------------------------------" + eol
+    DumperObject = Iriven_CSVDumper()
+    destFile = os.path.join(currentdir , 'Outputs',  'CreatedFile.csv')
+    DumperObject.setDestination(destFile)
+    DumperObject.save(ParserObject.searchRows(("admin" , "root")))
+    s = "----------------------------------------------------------" + EOL
+    for row in ParserObject.searchRows(("admin","staff")):
+        s += "Server : " + row[0] + EOL
+        s += "User : " + row[1] + EOL
+        s += "Password : " + row[3] + EOL
+        s += "HomeDir : " + row[2] + EOL
+        s += "----------------------------------------------------------" + EOL
         print(s)
